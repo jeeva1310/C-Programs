9d199684109a7d08685f1d8432828a50aaabfaa7
jeeva1310 <jeevaramamoorthi2007@gmail.com>
2025-08-26 09:55:10 -0700

    calculate division given number

---
+++ division.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+void main(){
+	int number1,number2;
+	float division=0;
+	printf("Enter a number: ");
+	scanf("%d",&number1);
+	printf("Enter another number: ");
+	scanf("%d",&number2);
+	division = number1 * number2;
+	printf("Sum of the given number is: %f.",division);
+}
